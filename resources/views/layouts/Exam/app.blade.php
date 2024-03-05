152000dd4a735fdd7ef91b9c9de52fb4792ce100
teckpy <aakashupadhyay.25@gmail.com>
2024-03-05 11:55:30 +0530

    ftr

--- resources/views/layouts/Exam/app.blade.php
+++ resources/views/layouts/Exam/app.blade.php
@@ -12,6 +12,9 @@
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('Admin/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('Admin/dist/css/adminlte.min.css') }}">
     <script src="{{ asset('Admin/js/jquery-3.7.1.min.js') }}"></script>
@@ -31,9 +34,8 @@
 
     </div>
 
-    <footer class="main-footer" style="text-align: :center">
-        <strong>Copyright &copy; 2014-2024 <a
-                href="https://goswamisirclasses.in">GoswamiSirAndSahuSirClasses</a>.</strong>
+    <footer class="fixed-bottom text-center bg-light">
+        Copyright &copy; 2014-2024 <a href="https://goswamisirclasses.in" class="text-gray">goswamisirclasses</a>.
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
             <b>Version</b> 3.2.0
